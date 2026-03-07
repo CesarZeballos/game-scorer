9233540ce6efff5b1a7223681ded4f3eafaf96f8
Cesar Zeballos <126284948+CesarZeballos@users.noreply.github.com>
2026-03-07 18:09:57 -0300

    added toast with btn

--- src/composables/useToast.ts
+++ src/composables/useToast.ts
@@ -45,7 +45,66 @@ export default function useToast() {
     $q.notify(props);
   };
 
+  const showToastWithBtn = (
+    message: string,
+    type: 'success' | 'error' | 'warning',
+    btnLabel: string,
+    onClick: () => void,
+    showIcon?: boolean,
+  ) => {
+    const position = 'top';
+    let color = '';
+    let textColor = '';
+    let icon = '';
+
+    switch (type) {
+      case 'success':
+        color = 'teal-1';
+        textColor = 'teal-6';
+        icon = 'o_check_circle';
+        break;
+      case 'error':
+        color = 'deep-orange-1';
+        textColor = 'deep-orange-7';
+        icon = ionWarningOutline;
+        break;
+      case 'warning':
+        color = 'yellow-1';
+        textColor = 'yellow-10';
+        icon = ionWarningOutline;
+        break;
+      default:
+        color = 'positive';
+        textColor = 'positive';
+        icon = 'o_check_circle';
+        break;
+    }
+
+    const props: QNotifyOptions = {
+      message: message,
+      color: color,
+      textColor: textColor,
+      position: position,
+      icon: showIcon ? icon : '',
+      actions: [
+        {
+          label: btnLabel,
+          color: textColor,
+          noCaps: true,
+          style: 'text-decoration: underline; text-underline-offset: 2px;',
+          handler: () => {
+            onClick();
+          },
+        },
+      ],
+      classes: 'border-radius-4'
+    };
+
+    $q.notify(props);
+  };
+
   return {
     showToast,
+    showToastWithBtn,
   };
 }
